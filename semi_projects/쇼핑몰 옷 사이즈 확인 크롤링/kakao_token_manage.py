921ce4e2a43458086ca83982f69eede09168cc0b
ramie <rambotty@gmail.com>
2021-08-26 23:07:12 +0900

    Add codes and files

---
+++ semi_projects/쇼핑몰 옷 사이즈 확인 크롤링/kakao_token_manage.py
@@ -0,0 +1,109 @@
+from os import access
+import requests
+import json
+import sys
+import datetime
+
+
+
+
+rest_API_key = '03e461ff4bab0a5e4979c7d12a3f6706'
+redirect_uri = 'https://localhost.com/auth'
+code = "s4maYjRlCA7GgBKwaPvIsGA6xnxCEmdPBG1mvimd_9DBOi-ij68aNaiPcxvH-k3uy_rzRAopcFAAAAF7ge2w7A"
+# ------------------- 인증코드 받기 ---------------------#
+# API Key
+#웹에서 받은 코드
+# 인증 코드 받는 곳: https://kauth.kakao.com/oauth/authorize?client_id=03e461ff4bab0a5e4979c7d12a3f6706&redirect_uri=https://localhost.com/auth&response_type=code&scope=talk_message,friends
+
+def registerAccessToken():
+    url = 'https://kauth.kakao.com/oauth/token'
+    data = {
+        'grant_type': 'authorization_code',
+        'client_id': rest_API_key,
+        'redirect_uri': redirect_uri,
+        'code': code
+    }
+
+    response = requests.post(url, data=data)
+    tokens = response.json()
+
+    with open("kakao_code.json", 'w') as fp:
+        json.dump(tokens, fp)
+    return(tokens)
+
+def sendMessageToMe(message):
+    temp = open("kakao_code.json", 'r')
+    tokens = json.load(temp)
+    current_time = "현재 시간 " + str(datetime.datetime.now())[:-7] + ".\n" + message
+
+    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+    headers = {
+        "Authorization" : "Bearer " + tokens['access_token']
+    }
+    data = {
+        "template_object" : json.dumps({
+            "object_type" : "text",
+            "text" : current_time,
+            "link" : {
+                "web_url" : "www.naver.com"
+            }
+        })
+    }
+    response = requests.post(url, headers=headers, data=data)
+    return response.status_code
+
+def sendMessageToFriend():
+    url = "https://kapi.kakao.com/v1/api/talk/friends/message/default/send" #api 주소
+    temp = open("kakao_code.json", 'r')
+    tokens = json.load(temp)
+    
+    header = {"Authorization": 'Bearer ' + tokens['access_token']}
+    uuid = ["itsem0@daum.net"]
+    uuidsData = {"itsem0@daum.net" : json.dumps(uuid)}    
+    
+    post = {
+        "object_type": "text",
+        "text" : "보내고자 하는 TEXT",
+        "link" : {
+            "web_url" : "http://naver.com",
+            "mobile_web_url" : "http://naver.com"
+        },
+        "button_title":"button title"
+    }
+    data = {"template_object": json.dumps(post)}
+    uuidsData.update(data)
+
+    return requests.post(url, headers=header, data=uuidsData).status_code
+
+def refreshToken():
+    url = "https://kapi.kakao.com/oauth/token"
+    temp = open("kakao_code.json", 'r')
+    tokens = json.load(temp)
+    
+    data = {
+        "grant_type": "refresh_token",
+        "client_id": rest_API_key,
+        "refresh_token": tokens['refresh_token']
+    }
+
+    response = requests.post(url, data=data)
+    tokens = response.json()
+    with open("kakao_code.json", "w") as fp:
+        json.dump(tokens, fp)
+    return response.status_code, tokens, response.json()
+
+# def checkTokenInfo():
+#     url = "https://kapi.kakao.com/v1/user/access_token_info"
+
+#     header = {
+#         "Authorization" : "Bearer " + access_token
+#     }
+
+#     data = {
+#         "id": 1869145849,
+#         "expires_in": 21599,
+#         "app_id": 629544
+#     }
+#     response = requests.post(url, data=data)
+#     tokens = response.json()
+#     return tokens
---
+++ semi_projects/쇼핑몰 옷 사이즈 확인 크롤링/shopping_mall_size_crawling.py
@@ -0,0 +1,39 @@
+import kakao_token_manage as ktm
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+class size_check_bot:
+    def __init__(self):
+        self.options = Options()
+        self.options.add_argument("--window-size=1024,768")
+        self.driver = webdriver.Chrome(executable_path="./chromedriver.exe", chrome_options=self.options)
+        self.webpage = "https://troubadour.kr/product/detail.html?product_no=1105"
+        self.driver.get(self.webpage)
+        time.sleep(3)
+
+    def find_path(self):
+        color_xpath = '//*[@id="df-product-detail"]/div/div[2]/div/div/div[1]/div[1]/ul/li[1]/ul/li[2]/ul/li/a'
+        text_xpath = '//*[@id="df-product-detail"]/div/div[2]/div/div/div[1]/div[1]/ul/li[2]/ul/li[2]/p/span'
+        self.color = self.driver.find_element_by_xpath(color_xpath)
+        self.text = self.driver.find_element_by_xpath(text_xpath)
+        self.color.click()
+        time.sleep(0.5)
+        self.size = self.driver.find_element_by_xpath('//*[@id="df-product-detail"]/div/div[2]/div/div/div[1]/div[1]/ul/li[2]/ul/li[2]/ul/li[2]/a/span')
+        self.size.click()
+        time.sleep(0.5)
+
+    def send_message(self):
+        check_text = self.text.text
+        if '품절' in check_text:
+            ktm.sendMessageToMe("재고 \"없음\"")
+        else:
+            ktm.sendMessageToMe("재고 \"있음\"")
+
+    def kill_process(self):
+        self.driver.quit()
+    
+    def run_process(self):
+        self.find_path()
+        self.send_message()
+        self.kill_process()
