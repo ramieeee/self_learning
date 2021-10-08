c857e34e0c79949867e332e65e9b84d55a1176e0
ramie <rambotty@gmail.com>
2021-10-08 21:15:40 +0900

    Add keyworkd and xpath

--- project/spam_mail_detection/dataset_17.py
+++ project/spam_mail_detection/dataset_17.py
@@ -3,4 +3,10 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome("./chromedriver.exe")
 driver.get("https://www.phishtank.com/")
+
+keyword = "instagram.com" # 검색어
+elem = driver.find_element_by_xpath('//*[@id="maincol"]/div/div[2]/form/p/input[1]')
+elem.send_keys(keyword)
+elem.send_keys(Keys.RETURN)
+
 # driver.quit()
