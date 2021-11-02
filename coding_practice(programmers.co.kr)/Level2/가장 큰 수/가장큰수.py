7cc8fc818da0aa03994f850a546c45cff7f3fc86
ramie <rambotty@gmail.com>
2021-11-02 13:51:15 +0900

    Add first Level2 coding problem

---
+++ coding_practice(programmers.co.kr)/Level2/가장 큰 수/가장큰수.py
@@ -0,0 +1,23 @@
+num = [3, 30, 34, 5, 9]
+num = [str(i) for i in num]
+
+temp = []
+answer = []
+for i in num:
+    temp.append(i[0])
+
+# while num:
+#     if max(num):
+
+# def solution(numbers):
+#     answer = ''
+#     return answer
+
+# print(num)
+# for i in 
+
+# 1. max 찾고
+# 2. 자리수가 다를 시
+# max(len)
+# print(max(num))
+# print(num.count(max(num)))
