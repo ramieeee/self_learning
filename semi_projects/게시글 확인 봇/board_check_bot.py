c2df8998a748f45437bcc07b878721e5764ed4aa
ramie <rambotty@gmail.com>
2021-11-06 22:09:37 +0900

    Modify notice phrases and delete token so to be invisible

--- semi_projects/게시글 확인 봇/board_check_bot.py
+++ semi_projects/게시글 확인 봇/board_check_bot.py
@@ -2,10 +2,10 @@ import requests
 import time
 import sys
 from bs4 import BeautifulSoup
-import atexit
 
 TARGET_URL = 'https://notify-api.line.me/api/notify'
-TOKEN = '9ySbbZxGDt33gWGefpBaX4oQfCZrAbtvQLq8GndcgNz'
+TOKEN = '' # token from line notify
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
 
 # 문화재청 url
 url1 = 'https://www.cha.go.kr/multiBbz/selectMultiBbzList.do?bbzId=newexam&mn=NS_01_06'
@@ -13,10 +13,6 @@ url1 = 'https://www.cha.go.kr/multiBbz/selectMultiBbzList.do?bbzId=newexam&mn=NS
 url2 = 'https://www.nrich.go.kr/kor/boardList.do?menuIdx=285&bbscd=40'
 # 경기문화재연구원 url
 url3 = 'https://gjicp.ggcf.kr/archives/category/notice'
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
-
-# 1. 라인 API 키 발금
-# 2. 본인 윈도우 계정 이름 넣기(path_dir에)
 
 def send_Line(text):
     try:
@@ -57,10 +53,10 @@ def one(url):
     try:
         if check_text(file_name, num1) == False:
             write_over(file_name, num1)
-            send_Line('문화재청 웹페이지에 새 개시글이 등록되었습니다. 데헷뿌잉뀨 ^ -^/')
+            send_Line('문화재청 웹페이지에 새 개시글이 등록되었습니다.')
     except FileNotFoundError:
         write_over(file_name, num1)
-        send_Line('문화재청 데이터를 새로 등록합니다. \n폴더 내 .data 파일은 건들지 말아주세요. 데헷뿌잉뀨 ^ -^/')
+        send_Line('문화재청 데이터를 새로 등록합니다.')
 
 def two(url):
     # 국립문화재연구소 새 글 추출
@@ -76,10 +72,10 @@ def two(url):
     try:
         if check_text(file_name, num2) == False:
             write_over(file_name, num2)
-            send_Line('국립문화재연구소 웹페이지에 새 개시글이 등록되었습니다. 데헷뿌잉뀨 ^ -^/')
+            send_Line('국립문화재연구소 웹페이지에 새 개시글이 등록되었습니다.')
     except FileNotFoundError:
         write_over(file_name, num2)
-        send_Line('국립문화재연구소 데이터를 새로 등록합니다. \n폴더 내 .data 파일은 건들지 말아주세요. 데헷뿌잉뀨 ^ -^/')
+        send_Line('국립문화재연구소 데이터를 새로 등록합니다.')
 
 def three(url):
     # 경기문화재연구원 새 글 추출
@@ -96,10 +92,10 @@ def three(url):
     try:
         if check_text(file_name, num3) == False:
             write_over(file_name, num3)
-            send_Line('경기문화재연구원 웹페이지에 새 개시글이 등록되었습니다. 데헷뿌잉뀨 ^ -^/')
+            send_Line('경기문화재연구원 웹페이지에 새 개시글/이 등록되었습니다.')
     except FileNotFoundError:
         write_over(file_name, num3)
-        send_Line('경기문화재연구원 데이터를 새로 등록합니다. \n폴더 내 .data 파일은 건들지 말아주세요. 데헷뿌잉뀨 ^ -^/')
+        send_Line('경기문화재연구원 데이터를 새로 등록합니다.')
 
 send_Line('프로그램 시작\n버그나 에러는 ramieeee에게 문의하세요\n\ngithub: https://github.com/ramieeee/')
 
@@ -111,10 +107,10 @@ def main():
             two(url2)
             time.sleep(0.2)
             three(url3)
-            time.sleep(2)
+            time.sleep(10)
     except:
-        send_Line('프로그램 강제 종료')
+        send_Line('프로그램을 강제 종료합니다.\n\n원하지 않는 종료라면 개발자에게 문의하세요\n\n')
 
 if __name__ == "__main__":
     main()
-    send_Line('또 만나요. 쀼쮸쀼쮸')
+    send_Line('또 만나요.')
