fc7c94f598c54ed47d0e7a72f0f4579fd715a27c
ramie <rambotty@gmail.com>
2022-01-11 10:37:53 +0900

    modify path index slicing

--- my_memo/work_related/test.py
+++ my_memo/work_related/test.py
@@ -9,13 +9,29 @@ import os
 arg = sys.argv[1]
 # raise an error when system argument is more or less than 1.
 if len(sys.argv) != 2:
-    raise SystemExit("   Note: test.py takes only 1 argument\n   Try typing in \"python3 test.py [file_name]\"")
+    raise SystemExit("   note: the script takes only 1 argument\n   try typing in \"python superset.py [file_name]\"")
+
+# path index slicing
+if arg == "/":
+    raise SystemExit("   note: filename missing")
+elif arg[-1] == "/":
+    raise SystemExit("   note: filename missing")
+elif "/" in arg and len(arg) > 1:
+    idx = 0
+    for i in range(len(arg), 0, -1):
+        idx += 1
+        if arg[i-1] == "/":
+            break
+    idx = idx * -1
+    path = arg[0:idx+1]
+else:
+    path = "./"
 
 # raise an error when there is no such file
-path = "./"
+file_arg = arg[idx+1:]
 file_list = os.listdir(path)
-if arg not in file_list:
-    raise SystemExit("   %s file does not exist in this directory" %arg)
+if file_arg not in file_list:
+    raise SystemExit("   %s file does not exist in this directory" %file_arg)
 
 
 def main():
@@ -23,14 +39,15 @@ def main():
     lines = f.readlines()
     count = 0
 
-#   with open(arg.1)
+#   with open(arg.dump)
     for line in lines:
         if "EXEC CICS" in line:
 
             count += 1
     if count == 0:
-        print("There is no \'EXEC CICS\' in the file")
-    print(count)
+        print("No \'EXEC CICS\' phrase in the file")
+    else:
+        print("total %d EXEC CICS phrase(s) in the file" %count)
     f.close()
 
 if __name__ == "__main__":
