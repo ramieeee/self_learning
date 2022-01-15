8bf631011df606f9c85b52485de739f576b51b55
ramie <rambotty@gmail.com>
2022-01-15 16:25:23 +0900

    Add options and functions

--- my_memo/work_related/get_superset.py
+++ my_memo/work_related/get_superset.py
@@ -9,98 +9,148 @@ import sys
 import os
 
 # raise an error when system argument is more or less than 1.
-def check_argv():
-    if len(sys.argv) != 2:
-        raise SystemExit("   note: the script takes only 1 argument\n   try typing in \"python superset.py [file_name]\"")
+##################################### consider option####################
+def check_argv_len():
+	if len(sys.argv) < 2 or len(sys.argv) > 3:
+		raise SystemExit("   note: the script takes only 1 argument and an option\n   try \'python <py_script> --help\' for details\"")
+	if len(sys.argv) == 2:
+		return 2
+	return 3
 
 # path index slicing
 def path_set(arg):
-    if arg == "/" or arg[-1] == "/":
-        raise SystemExit("   note: filename missing")
-    elif "/" in arg and len(arg) > 1:
-        idx = 0
-        for i in range(len(arg), 0, -1):
-            idx += 1
-            if arg[i-1] == "/":
-                break
-        idx = idx * -1
-        path = arg[0:idx+1]
-    else:
-        path = "./"
-    return path
+	if arg == "/" or arg[-1] == "/":
+		raise SystemExit("   note: filename missing")
+	elif "/" in arg and len(arg) > 1:
+		idx = 0
+		for i in range(len(arg), 0, -1):
+			idx += 1
+			if arg[i-1] == "/":
+				break
+		idx = idx * -1
+		path = arg[0:idx+1]
+	else:
+		path = "./"
+	return path
 
 # pharsing file name
 def get_file_name(arg):
-    idx = 0
-    for i in range(len(arg), 0, -1):
-        idx += 1
-        if arg[i-1] == "/":
-            break
-    idx = idx * -1
-    file_name = arg[idx:]
-    return file_name
+	if "/" in arg:
+		for i in range(len(arg), 0, -1):
+			if arg[i-1] == "/":
+				file_name = arg[i:]
+				break
+		return file_name
+	else:
+		return arg
+
 
 # check dumpfile in the directory
 def check_dumpfile(file_arg, path):
-    if file_arg+".dump" in os.listdir(path):
-        raise SystemExit("%s.dump file already exits in directory\nexiting the program" %file_arg)
+	if file_arg+".dump" in os.listdir(path):
+		raise SystemExit("\'%s.dump\' file already exits in directory\nexiting the program" %file_arg)
 
 # raise an error when there is no such file
 def check_file(file_name, path):
-    file_list = os.listdir(path)
-    if file_name not in file_list:
-        raise SystemExit("   %s file does not exist in this directory" %file_name)
+	file_list = os.listdir(path)
+	if file_name not in file_list:
+		raise SystemExit("   \'%s\' file does not exist in this directory" %file_name)
 
-def help_print(arg):
-    if arg == "--help":
-        print("\nit detects all EXEC CICS statements and dump into <file_name.dump> file\n")
-        print("usage: python <py_script> <target_file> <option>")
-        print("  <py_script>        python script file to be executed")
-        print("  <target_file>      file from which you need to source out EXEC CICS\n")
-        print("options:")
-        print("  -d, default        dumping only")
-        print("  -l, linear         dumping in a straight line")
-        return 1
-    return 0
+def help_print():
+		print("\nit detects all EXEC CICS statements and dump into <file_name.dump> file\n")
+		print("usage: python <py_script> <target_file> <option>")
+		print("  <py_script>        python script file to be executed")
+		print("  <target_file>      file from which you need to source out EXEC CICS\n")
+		print("options:")
+		print("  -d, default        dumping only. dumps in original form")
+		print("  -l, linear         dumping in a straight line")
+
+def exec_d_option(path, file_name):
+	f = open(path+file_name, "r")
+	lines = f.readlines()
+	EXEC_count = 0
+
+	buff = []
+	for i in range(len(lines)):
+		idx = i
+		if "EXEC" in lines[idx] and "-EXEC" not in lines[idx] and lines[idx][6] != "*":
+			buff.append(lines[idx])
+			idx += 1
+			while True:
+				if "END-EXEC" in lines[idx]:
+					buff.append(lines[idx])
+					break
+				elif len(lines[idx]) > 6 and lines[idx][6] == "*":
+					idx += 1
+					continue
+				buff.append(lines[idx])
+				idx += 1
+			EXEC_count += 1
+	if EXEC_count == 0:
+		print("no \'EXEC CICS\' statement in the file") 
+	else:
+		print("total %d EXEC CICS statement(s) in the file\nDUMPING SUCCESSFUL into %s.dump" %(EXEC_count,file_name))
+		with open(path+file_name+".dump", "a") as w_file:
+			for i in buff:
+				w_file.write(i)
+	f.close()
+
+def exec_l_option(path, file_name):
+	f = open(path+file_name, "r")
+	lines = f.readlines()
+
+	buff = []
+	for i in range(len(lines)):
+		temp = []
+		idx = i
+		if "EXEC" in lines[idx] and "-EXEC" not in lines[idx] and lines[idx][6] != "*":
+			temp.append(lines[idx][6:].strip())
+			idx += 1
+			while True:
+				if "END-EXEC" in lines[idx]:
+					temp.append(lines[idx][6:].strip())
+					break
+				elif len(lines[idx]) > 6 and lines[idx][6] == "*":
+					idx += 1
+					continue
+				temp.append(lines[idx][6:].strip())
+				idx += 1
+			buff.append(" ".join(temp))
+
+	if len(buff) == 0:
+		print("no \'EXEC CICS\' statement in the file") 
+	else:
+		print("total %d EXEC CICS statement(s) in the file\nDUMPING SUCCESSFUL into %s.dump" %(len(buff),file_name))
+		buff = "\n".join(buff)
+		with open(path+file_name+".dump", "w") as w_file:
+			w_file.write(buff)
+	f.close()
 
 def main():
-    check_argv()
-    arg = sys.argv[1]
-    path = path_set(arg)
-    file_name = get_file_name(arg)
-    check_file(file_name, path)
-    check_dumpfile(file_name, path)
+	len_argv = check_argv_len()
+	arg = sys.argv[1]
+	if len_argv == 3:
+		option = sys.argv[2]
 
-    f = open(arg, "r")
-    lines = f.readlines()
-    EXEC_count = 0
+	if "--help" in arg:
+		help_print()
+		exit(0)
+	elif len_argv == 3 and "--help" in option:
+		help_print()
+		exit(0)
+	path = path_set(arg)
+	file_name = get_file_name(arg)
+	check_file(file_name, path)
+	check_dumpfile(file_name, path)
 
-    # dumping EXEC CICS phrases into .dump file
-    for i in range(len(lines)):
-        t_idx = i
-        # check EXEC CICS statement and ignore comments
-        if "EXEC CICS" in lines[t_idx] and lines[t_idx][6] != "*":
-            # append into .dump file
-            while True:
-                if "END-EXEC" in lines[t_idx]:
-                    with open(file_name+".dump", "a") as dump_file:
-                        dump_file.write(lines[t_idx][6:].strip() + " ")
-                    break
-                with open(file_name+".dump", "a") as dump_file:
-                    dump_file.write(lines[t_idx][6:].strip() + " ")
-                t_idx += 1
-            with open(file_name+".dump", "a") as dump_file:
-                dump_file.write("\n")
-            EXEC_count += 1
-
-    # check if EXEC CICS statement is in the file
-    if EXEC_count == 0:
-        print("no \'EXEC CICS\' statement in the file")
-    else:
-        print("total %d EXEC CICS statement(s) in the file\nDUMPING SUCCESSFUL" %EXEC_count)
-
-    f.close()
-
+	# when takes in option
+	if len_argv == 3 and option == "-l":
+		exec_l_option(path, file_name)
+	elif len_argv == 2 or (len_argv == 3 and option == "-d"):
+		exec_d_option(path, file_name)
+	else:
+		help_print()
+		exit(0)
 if __name__ == "__main__":
-    main()
-    exit(0)
+	main()
+	exit(0)
